42bac67c713c1ef84c8d1073dd4aa74c58fc8a23
Hajin Kim <kimhajin@yonsei.ac.kr>
2024-05-14 18:30:42 +0900

    feat: implement image preview and upload button

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [imageFile, setImageFile] = useState<File | undefined>(undefined);
+  const [imagePreview, setImagePreview] = useState<string | undefined>(undefined);
 
-  const uploadFile = async (file: File) => {
-    const formData = new FormData()
-    formData.append('file', file)
+  const removeImage = () => {
+    setImageFile(undefined);
+    setImagePreview(undefined);
+  }
+
+  const holdImage = (file: File | null | undefined) => {
+    if (!file) {
+      removeImage();
+      return;
+    }
+
+    setImageFile(file);
+
+    const fileRead = new FileReader();
+    fileRead.onload = function () {
+      setImagePreview(fileRead.result as string);
+    };
+
+    fileRead.readAsDataURL(file);
+  };
+
+  const uploadImageIfHeld = async (file: File | undefined) => {
+    if (!file)
+      return;
+
+    const formData = new FormData();
+    formData.append('file', file);
     const response = await fetch('http://localhost:8080/', {
       method: 'POST',
       body: formData,
-    })
-    console.log(response)
+    });
+    console.log(response);
   }
+
   return (
     <>
+      <h1>노려라 금손</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {imagePreview && <img alt={imagePreview} src={imagePreview} width={250} height={250}/>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <input
+          accept={"image/*"}
+          type="file"
+          onChange={(e) => holdImage(e.target.files?.item(0))}/>
+        <button onClick={() => uploadImageIfHeld(imageFile)}>Upload</button>
       </div>
-      <input type="file" onChange={(e) => uploadFile(e.target.files![0])} />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
